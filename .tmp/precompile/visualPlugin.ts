4f456244a144db10dd711e150a07aa5c26db43bd
Scottie Milton (MINDTREE LIMITED) <v-scmilt@microsoft.com>
2017-11-20 11:28:53 -0800

    initial commit

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var powerBIVisual1D28CB10BD8040A98F545F6699A510D2_DEBUG = {
+        name: 'powerBIVisual1D28CB10BD8040A98F545F6699A510D2_DEBUG',
+        displayName: 'PowerBIVisual',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.9.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.powerBIVisual1D28CB10BD8040A98F545F6699A510D2.Visual(options),
+        custom: true
+    };
+}
